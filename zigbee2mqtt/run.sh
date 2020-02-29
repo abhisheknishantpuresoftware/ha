a273602ac9798f7baf0eb70dd2a409e54f0a9529
Viet Tien <ttvtien@gmail.com>
2020-02-29 11:01:31 +0700

    add script to check device serial port

--- zigbee2mqtt/run.sh
+++ zigbee2mqtt/run.sh
@@ -5,12 +5,20 @@ DATA_PATH=/share/zigbee2mqtt
 
 ZIGBEE_DEBUG=$(jq --raw-output ".zigbee_debug // empty" $CONFIG_PATH)
 ZIGBEE_DEVICES=$(jq --raw-output ".zigbee_devices // empty" $CONFIG_PATH)
+ZIGBEE_PORT=$(jq --raw-output ".port // empty" $CONFIG_PATH)
 
 # Create config directory
 mkdir -p $DATA_PATH
 
 # Modify config
 cat "$CONFIG_PATH" | jq 'del(.zigbee_debug)' | jq 'del(.zigbee_devices)' | jq '. + {"homeassistant": true}' > $DATA_PATH/configuration.yaml
+
+#Script to check device port
+if [ ! -f $ZIGBEE_PORT ] ; then {
+   echo Zigbee device port not found. Please check your device and config
+   echo Will now stop!
+   exit
+}
 
 if [[ ! -z "$ZIGBEE_DEBUG" ]]; then
     echo "[Info] Zigbee debug logging enabled."
